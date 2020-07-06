2a01e437149e78fb9bfd106b46dd3d0c3d35b048
Arshad0786 <js07150715@gmail.com>
2020-07-06 17:42:36 +0800

    creating main file

---
+++ BinaryTree.py
@@ -0,0 +1,11 @@
+class TreeNode():
+    def __init__(self, val, root=None, leftNode = None, rightNode = None):
+        self.val = val
+        self.root = root
+        self.leftNode = leftNode
+        self.rightNode = rightNode
+
+class BinaryTree():
+    def __init__(self):
+        self.root = None
+        
