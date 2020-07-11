59c85935950f09d3e03a9f4a7861ff816d9d9434
Arshad0786 <js07150715@gmail.com>
2020-07-11 18:24:49 +0800

    Adding stack version of traversals

--- BinaryTree.py
+++ BinaryTree.py
@@ -19,6 +19,21 @@ class BinaryTree:
         self.root.rightNode.leftNode = TreeNode(60)
         self.root.rightNode.rightNode = TreeNode(70)
 
+    def initLeftLeaningTree(self):
+        self.root = TreeNode(0)
+        tracer = self.root
+        for i in range(10):
+            tracer.leftNode = TreeNode((i+1)*10)
+            tracer = tracer.leftNode
+
+    def initRightLeaningTree(self):
+        self.root = TreeNode(0)
+        tracer = self.root
+        for i in range(10):
+            tracer.rightNode = TreeNode((i+1)*10)
+            tracer = tracer.rightNode
+    
+
     def preorder_recursive(self, TreeNode):
         if not TreeNode:
             return []
@@ -52,12 +67,43 @@ class BinaryTree:
         output.append(TreeNode.val)
         return output
 
-    def preorder_stack(slef, TreeNode):
+    def preorder_stack(self, TreeNode):
         if not TreeNode:
             return []
         output = []
         stack = []
         stack.append(TreeNode)
         while stack:
-            pass
+            root = stack.pop()
+            output.append(root.val)
+            if root.rightNode:
+                stack.append(root.rightNode)
+            if root.leftNode:
+                stack.append(root.leftNode)
+        return output
+    
+    def inorder_stack(self, TreeNode):
+        if not TreeNode:
+            return []
+        output = []
+        stack = []
+        root = TreeNode
+        while stack or root:
+            while(root):
+                stack.append(root)
+                root = root.leftNode
+            root = stack.pop()
+            output.append(root.val)
+            root = root.rightNode
+        return output
+    
+    def postorder_stack(self, TreeNode):
+        if not TreeNode:
+            return []
+        output = []
+        stack = []
+        
+            
+            
+            
         
--- BinaryTree_test.py
+++ BinaryTree_test.py
@@ -22,6 +22,37 @@ class BinaryTreeTest(unittest.TestCase):
             temp.postorder_recursive(temp.root), [40, 50, 20, 60, 70, 30, 10]
         )
 
+    def test_preorder_stack(self):
+        temp = BinaryTree()
+        temp.initExampleTree()
+        self.assertEqual(temp.preorder_recursive(temp.root),temp.preorder_stack(temp.root))
+
+    def test_inorder_stack(self):
+        temp = BinaryTree()
+        temp.initExampleTree()
+        self.assertEqual(temp.inorder_recursive(temp.root),temp.inorder_stack(temp.root))
+    
+    def test_leftLeaningTreeTest(self):
+        temp = BinaryTree()
+        temp.initLeftLeaningTree()
+        result = []
+        for i in range(11):
+            result.append((i)*10)
+        self.assertEqual(temp.preorder_recursive(temp.root),result)
+        self.assertEqual(temp.inorder_recursive(temp.root),result[::-1])
+        self.assertEqual(temp.postorder_recursive(temp.root),result[::-1])
+
+    def test_rightLeaningTreeTest(self):
+        temp = BinaryTree()
+        temp.initRightLeaningTree()
+        result = []
+        for i in range(11):
+            result.append((i)*10)
+        self.assertEqual(temp.preorder_recursive(temp.root),result)
+        self.assertEqual(temp.inorder_recursive(temp.root),result)
+        self.assertEqual(temp.postorder_recursive(temp.root),result[::-1])
+        
+
 
 if __name__ == "__main__":
     unittest.main()
